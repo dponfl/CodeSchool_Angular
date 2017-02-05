bf301a531ef3bc99dfbab5fe21432b5a3a711e63
dmsch <dschukin@mail.ru>
2017-02-05 20:28:03 +0200

    up to slide 135

--- assets/js/modules/app.js
+++ assets/js/modules/app.js
@@ -7,7 +7,7 @@
     this.products = gems
   });
 
-  app.controller('PanelController', function () {
+  /*app.controller('PanelController', function () {
     this.tab = 1;
 
     this.selectTab = function (setTab) {
@@ -17,21 +17,56 @@
     this.isSelected = function (checkTab) {
       return this.tab == checkTab;
     }
-  });
+  });*/
 
-  app.controller('ReviewController', function () {
+  /*app.controller('ReviewController', function () {
     this.review = {};
 
     this.addReview = function (product) {
       product.reviews.push(this.review);
       this.review = {};
     };
-  });
+  });*/
 
   app.directive('productTitle', function () {
     return {
       restrict: 'E',
       templateUrl: 'templates/product-title.html'
+    };
+  });
+
+  app.directive('submitReview', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/submit-review.html',
+      controller: function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+          product.reviews.push(this.review);
+          this.review = {};
+        };
+      },
+      controllerAs: 'reviewCtrl'
+    };
+  });
+
+  app.directive('productPanel', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/product-panel.html',
+      controller: function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+          this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+          return this.tab == checkTab;
+        }
+      },
+      controllerAs: 'panel'
     }
   });
 
